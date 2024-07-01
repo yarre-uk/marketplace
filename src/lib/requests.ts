ebdbda29438280ca28fa97f8f425da1033c49594
Yarre <yaroslav.syvukha@gmail.com>
2024-07-01 15:28:53 +0200

    feat: prerequisites for new backend on nest

--- src/lib/requests.ts
+++ src/lib/requests.ts
@@ -3,8 +3,6 @@ import { axiosClient } from './axios';
 import { NFTRequestResponse, bytes } from '@/types';
 
 export const fetchForAddress = async (address: bytes) => {
-  const response = await axiosClient.get<NFTRequestResponse>(
-    `/user/${address}`,
-  );
+  const response = await axiosClient.get<NFTRequestResponse>(`/nft/${address}`);
   return response.data.nfts;
 };
